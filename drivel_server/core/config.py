f0f3e253927dcdea01875817160424a1d4e5c75e
Noa Onoszko <noaono@hotmail.com>
2024-02-15 12:36:11 +0100

    Fix path to secrets in config
    This is a bugfix. The bug, a typo in the form of missing suffixes
    to the filenames, was introduced by the refactoring PR (and specifically
    this commit: cb40f5b).

--- drivel_server/core/config.py
+++ drivel_server/core/config.py
@@ -38,8 +38,8 @@ class Settings(BaseSettings):
 
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "drivel-server"
-    SERVICE_ACCOUNT_KEY_FILE: str = "/run/secrets/google-service-account-key"
-    OPENAI_API_KEY_FILE: str = "/run/secrets/openai-key"
+    SERVICE_ACCOUNT_KEY_FILE: str = "/run/secrets/google-service-account-key.json"
+    OPENAI_API_KEY_FILE: str = "/run/secrets/openai-key.txt"
 
     GPT_MODEL: GPT_MODELS = "gpt-3.5-turbo"
     STT_MODEL: str = "whisper-1"
