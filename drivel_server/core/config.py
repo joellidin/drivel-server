1ee5b1d91fc799ded8b1c4b984387f5b2cd0536f
Joel Lidin <joellidin@gmail.com>
2024-02-23 14:12:18 +0100

    Add openai org id to base settings config

--- drivel_server/core/config.py
+++ drivel_server/core/config.py
@@ -48,6 +48,7 @@ class Settings(BaseSettings):
 
     gcp_project_number: str
     gcp_secret_name_openai_key: str
+    gcp_secret_name_openai_organization_id: str
     secrets_folder: str
 
     # The environment variable `ENV` is set to prod in the deploy script. In
@@ -66,6 +67,12 @@ class Settings(BaseSettings):
     def openai_api_key_file(self) -> str:
         """Construct path to OpenAI API key file."""
         return f"{self.secrets_folder}/{self.gcp_secret_name_openai_key}"
+
+    @computed_field
+    @property
+    def openai_organization_id_file(self) -> str:
+        """Construct path to OpenAI API key file."""
+        return f"{self.secrets_folder}/{self.gcp_secret_name_openai_organization_id}"
 
 
 settings = Settings()
